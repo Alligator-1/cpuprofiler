597a3101f403ca358edeb85ccbe87a1db3bce5e6
Alligator-1 <alligator.ak@gmail.com>
2024-11-16 21:14:41 +0500

    improved the way to call the profiler code

--- dll/profiler_proxy.pas
+++ dll/profiler_proxy.pas
@@ -1,30 +1,25 @@
 unit profiler_proxy;
 {$mode objfpc}
+{$define NOPROFILING}
 
 interface
 
-const profiler_dll = 'profiler.dll';
+const
+  profiler_dll = 'profiler.dll';
+
+var
+  LibHandle: TLibHandle;
+  profiler_enter: TProcedure=TProcedure(Pointer($10101010)); public name 'profiler_enter';
+  profiler_leave: TProcedure=TProcedure(Pointer($20202020)); public name 'profiler_leave';
 
 procedure profiler_init; external profiler_dll name 'profiler_init';
 procedure profiler_reset; external profiler_dll name 'profiler_reset';
 
-procedure profiler_enter; public name 'profiler_enter';
-procedure profiler_leave; public name 'profiler_leave';
-
 implementation
 
-procedure _profiler_enter; external profiler_dll name 'profiler_enter';
-procedure _profiler_leave; external profiler_dll name 'profiler_leave';
-
-procedure profiler_enter; assembler; nostackframe;
-asm
-  jmp _profiler_enter
-end;
-
-procedure profiler_leave; assembler; nostackframe;
-asm
-  jmp _profiler_leave
-end;
+initialization
+  LibHandle:=LoadLibrary(profiler_dll);
+  profiler_enter:=TProcedure(GetProcAddress(LibHandle, 'profiler_enter'));
+  profiler_leave:=TProcedure(GetProcAddress(LibHandle, 'profiler_leave'));
 
 end.
-
